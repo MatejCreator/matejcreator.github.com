1b806e76b7040a7c43d1f684675efd4366767e21
Matej Palco <palcomatej04@gmail.com>
2023-12-08 16:55:31 +0100

    Absolute new web page

--- 1oldweb/test/test_script.js
+++ 1oldweb/test/test_script.js
@@ -1,5 +1,22 @@
-function add() {
-    let text = "Hello";
-    text += " world!";
-    document.write(text);
+const images = [
+    { src: "samurai.jpg", url: "https://c4.wallpaperflare.com/wallpaper/653/194/366/samurai-japan-japanese-digital-art-artwork-hd-wallpaper-preview.jpg"}
+]; // Add more image URLs as needed
+let currentIndex = 0;
+
+function showImage(index) {
+  const imgElement = document.getElementById("currentImg");
+  imgElement.src = images[index];
 }
+
+function nextImage() {
+  currentIndex = (currentIndex + 1) % images.length;
+  showImage(currentIndex);
+}
+
+function prevImage() {
+  currentIndex = (currentIndex - 1 + images.length) % images.length;
+  showImage(currentIndex);
+}
+
+// Show the initial image
+showImage(currentIndex);
